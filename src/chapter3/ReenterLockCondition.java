ce31df5c441becbfc5e1485f4c03f47e309cd8df
王鹏 <javawp@icloud.com>
2016-10-23 15:09:06 +0800

    重入锁配合condition一起使用

--- src/chapter3/ReenterLockCondition.java
+++ src/chapter3/ReenterLockCondition.java
@@ -11,12 +11,12 @@ public class ReenterLockCondition implements Runnable {
 	@Override
 	public void run() {
 		try {
-			lock.lock();
+			lock.lock(); // 当线程调用condition.await(); 需要先获取锁
 			/**
 			 * await()方法会使当前线程等待,同时释放当前锁,当其他线程使用signal()或者signalAll()方法时,
 			 * 线程会重新获得锁并继续执行.或者当线程被中断时,也能跳出等待.
 			 */
-			condition.await();
+			condition.await(); // 调用之后, 会释放锁
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		} finally {
@@ -30,9 +30,9 @@ public class ReenterLockCondition implements Runnable {
 		t1.start();
 		Thread.sleep(2000);
 		// 通知线程t1继续执行
-		lock.lock();
+		lock.lock(); // 调用唤醒方法之前, 需要先获取锁
 		condition.signal();
-		lock.unlock();
+		lock.unlock(); // 唤醒之后, 需要释放锁, 不然等待的线程就算被唤醒了, 也无法继续执行, 因为获取不到锁.
 	}
 
 }
